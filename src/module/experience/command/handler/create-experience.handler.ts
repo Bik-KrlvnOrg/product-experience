b1952e9ceb34c651999ab3823ba21547ed8a9c9c
Bik-Krlvn <kelvinbirikorang@gmail.com>
2022-01-19 01:53:44 +0000

    refactor code

--- src/module/experience/command/handler/create-experience.handler.ts
+++ src/module/experience/command/handler/create-experience.handler.ts
@@ -1,13 +1,6 @@
-import {
-  CommandHandler,
-  EventBus,
-  EventPublisher,
-  ICommandHandler,
-} from '@nestjs/cqrs';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { CreateExperienceCommand } from '../impl';
 import { Logger } from '@nestjs/common';
-import { ExistEntityService } from '../../../../libs/service';
-import { ExperienceEntity } from '../../../../entities';
 import { ExperienceProjection } from '../../projection/experience.projection';
 
 @CommandHandler(CreateExperienceCommand)
@@ -16,17 +9,14 @@ export class CreateExperienceHandler
 {
   constructor(
     private readonly projection: ExperienceProjection,
-    private readonly existEntityService: ExistEntityService,
-    private readonly eventEventBus: EventBus,
     private readonly publisher: EventPublisher,
   ) {}
 
   logger = new Logger(this.constructor.name);
 
   async execute(command: CreateExperienceCommand): Promise<any> {
-    this.logger.log(command.experienceDto, 'CreateExperienceCommand');
     const { experienceDto } = command;
-    await this.existEntityService.exists(ExperienceEntity, experienceDto.name);
+    this.logger.log(experienceDto, 'CreateExperienceCommand');
     const projection = await this.projection.onCreate(experienceDto);
     const context = this.publisher.mergeObjectContext(projection);
     context.commit();
--- src/module/experience/service/experience.service.ts
+++ src/module/experience/service/experience.service.ts
@@ -3,14 +3,21 @@ import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetExperienceQuery, GetExperiencesQuery } from '../query/impl';
 import { CreateExperienceDto, UpdateExperienceDto } from '../dto';
 import { CreateExperienceCommand } from '../command/impl';
+import { ExistEntityService } from '../../../libs/service';
+import { ExperienceEntity } from '../../../entities';
 
 @Injectable()
 export class ExperienceService {
   constructor(
+    private readonly existEntityService: ExistEntityService,
     private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
   ) {}
   async create(createExperienceDto: CreateExperienceDto) {
+    await this.existEntityService.exists(
+      ExperienceEntity,
+      createExperienceDto.name,
+    );
     return await this.commandBus.execute(
       new CreateExperienceCommand(createExperienceDto),
     );
