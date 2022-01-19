57219b0f01c75a3bff0da786393ed97bb4248e0e
Bik-Krlvn <kelvinbirikorang@gmail.com>
2022-01-19 02:12:44 +0000

    add transactional decorator

--- src/module/experience/projection/experience.projection.ts
+++ src/module/experience/projection/experience.projection.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Experience } from '../domain/model';
 import { CreateExperienceDto } from '../dto';
 import { ExperienceRepository } from '../domain/repository';
+import { Transactional } from 'typeorm-transactional-cls-hooked';
 
 @Injectable()
 export class ExperienceProjection {
   constructor(private readonly repository: ExperienceRepository) {}
+  @Transactional()
   async onCreate(dto: CreateExperienceDto) {
     const entity = this.repository.create(dto);
     const save = await this.repository.save(entity);
