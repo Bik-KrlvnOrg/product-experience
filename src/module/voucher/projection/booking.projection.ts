ccc52d4ffcfa9e2f18b5992e5585becd382376ee
Bik-Krlvn <kelvinbirikorang@gmail.com>
2022-01-20 01:47:39 +0000

    add missing transactional decorator

--- src/module/voucher/projection/booking.projection.ts
+++ src/module/voucher/projection/booking.projection.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Booking } from '../domain/Booking';
 import { AppointmentRepository } from '../domain/repository';
+import { Transactional } from 'typeorm-transactional-cls-hooked';
 
 @Injectable()
 export class BookingProjection {
   constructor(private readonly repository: AppointmentRepository) {}
 
+  @Transactional()
   async bookAppointment(input) {
     const entity = this.repository.create(input);
     const save = await this.repository.save(entity);
